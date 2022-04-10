2067f9f08f0472173681464d8ea1c0de665f7865
xanmalodon <xanmalodon@gmail.com>
2022-04-10 18:16:51 +0200

    Connection à une BDD MySQL pour valider la connexion

--- LoginModule/src/main/java/com/login/Login.java
+++ LoginModule/src/main/java/com/login/Login.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.login.dao.LoginDao;
+
 /**
  * Servlet implementation class Login
  */
@@ -18,8 +20,9 @@ public class Login extends HttpServlet {
 		String uname = request.getParameter("uname");
 		String pass = request.getParameter("pass");
 		
-		if (uname.equals("louis") && pass.equals("louis")) {
-			
+		LoginDao dao = new LoginDao(); 
+		
+		if(dao.check(uname, pass)) {
 			HttpSession session = request.getSession();
 			session.setAttribute("username", uname);
 			
--- LoginModule/src/main/java/com/login/dao/LoginDao.java
+++ LoginModule/src/main/java/com/login/dao/LoginDao.java
@@ -1,9 +1,36 @@
 package com.login.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginDao {
 
-	public boolean check(String uname, String password) {
+	String sql = "select * from login where username=? and pass=?";
+	String url = "jdbc:mysql://localhost:3306/louis?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+	String username = "root";
+	String password = "root";
+	
+	public boolean check(String uname, String pass) {
 		
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url,username,password);
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, uname);
+			st.setString(2, pass);
+			ResultSet rs = st.executeQuery();
+			if (rs.next()) {
+				return true ; 
+			}
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		return false ; 
