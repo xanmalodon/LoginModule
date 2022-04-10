2bb3f2be8f88fca26be8547f958236b852431770
xanmalodon <xanmalodon@gmail.com>
2022-04-10 14:22:36 +0200

    First Commit

---
+++ LoginModule/src/main/java/com/login/Login.java
@@ -0,0 +1,28 @@
+package com.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname = request.getParameter("uname");
+		String pass = request.getParameter("pass");
+		
+		if (uname.equals("louis") && pass.equals("louis")) {
+			response.sendRedirect("welcome.jsp");
+		}else {
+			response.sendRedirect("login.jsp");
+		}
+		
+	}
+
+}
